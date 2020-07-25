7a5ab882990d10421361cd56770a6017a2d36aca
Gabriel Ullmann <gabriel.ullmann@agenciagh.com.br>
2020-07-25 15:50:53 -0300

    base recommendation algorithm

--- src/cluster/recommend.py
+++ src/cluster/recommend.py
@@ -1,6 +1,20 @@
-class Recommend:
-    def __init__(self):
-        print("ok")
+from database import Database
+from dataframe import Dataframe
 
-    def test(self):
-        return "lorem ipsum"
+class Recommend:
+    def __init__(self, database, collection):
+        self.db = database
+        self.coll = collection
+
+    def get(self, clientId, month):
+        db = Database(None)
+        df = Dataframe(None)
+
+        query = { "clientId": clientId, "month": month }
+        result = db.doQuery(self.db, self.coll, query)
+        orders = df.createDf(result)
+        print(orders)
+
+if __name__ == "__main__":
+    rc = Recommend('tcc', 'processed')
+    rc.get(13047, 1)
