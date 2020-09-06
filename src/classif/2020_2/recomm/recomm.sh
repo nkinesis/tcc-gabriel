859b75e000dd3bc6d2b0f89537cc5f23ff6b30ce
Gabriel Ullmann <gabriel.ullmann@agenciagh.com.br>
2020-09-05 21:46:41 -0300

    ajuste script

--- src/classif/2020_2/recomm/recomm.sh
+++ src/classif/2020_2/recomm/recomm.sh
@@ -1,7 +1,7 @@
 cd build_ds
-#python 1_bs_client.py
-#python 2_bs_country.py
-#python 3_ratings.py
+python 1_bs_client.py
+python 2_bs_country.py
+python 3_ratings.py
 cd ..
 cd predict
 python recomm-train.py 3
